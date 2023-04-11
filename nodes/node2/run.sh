898fbc133ba293455d0024a60aa9e5cf4367bd3b
CarlosR <carlos.ramos@grupoimportia.com>
2023-04-11 19:05:29 +0800

    added websocket to the script

--- nodes/node2/run.sh
+++ nodes/node2/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 geth init --datadir . genesis.json
-geth --datadir . --http --http.addr 0.0.0.0 --http.port 8565 --authrpc.port 9565 --http.corsdomain "*" --dev --password password.txt --networkid 54321 --http.corsdomain '*' --ipcdisable
+geth --datadir . --http --http.addr 0.0.0.0 --http.port 8565 --authrpc.port 9565 --http.corsdomain "*" --ws --ws.port 8566 --ws.origins "*" --dev --password password.txt --networkid 54321 --http.corsdomain '*' --ipcdisable
